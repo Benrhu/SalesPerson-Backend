fa20f2b4500730e264b05b8afe0709f7d2423373
Benrhu <Benrhu@gmail.com>
2022-09-12 16:00:08 +0200

    Project's structure

---
+++ src/test/java/com/grouzy/Backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grouzy.Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
