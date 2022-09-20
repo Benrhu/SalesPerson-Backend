2074c9a5f5de60bf0847320aa58204d8bcb41109
Rubdh <Benrhu@gmail.com>
2022-09-20 12:21:33 +0200

    Api created

--- src/main/java/com/grouzy/Backend/Controllers/SalesPersonController.java
+++ src/main/java/com/grouzy/Backend/Controllers/SalesPersonController.java
@@ -1,6 +1,77 @@
 package com.grouzy.Backend.Controllers;
 
+import com.grouzy.Backend.Entities.SalesPerson;
+import com.grouzy.Backend.Entities.User;
+import com.grouzy.Backend.Repositories.SalesPersonRepository;
+import com.grouzy.Backend.Repositories.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
 public class SalesPersonController {
 
-    Sales
+    private SalesPersonRepository salesPersonRepository;
+    private final Logger log = LoggerFactory.getLogger(UserController.class);
+
+    public SalesPersonController(SalesPersonRepository salesPersonRepository){
+        this.salesPersonRepository = salesPersonRepository;
+    }
+
+    @GetMapping("/api/users")
+    public List<SalesPerson> findAll() {
+        return salesPersonRepository.findAll();
+    }
+
+    @GetMapping("/api/users/{id}")
+    public ResponseEntity<SalesPerson> findOneById(@PathVariable Long salesPersonId) {
+        Optional<SalesPerson> salesPersonOpt = salesPersonRepository.findById(salesPersonId);
+        if (salesPersonOpt.isPresent())
+            return ResponseEntity.ok(salesPersonOpt.get());
+        else
+            return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping("api/salesPerson")
+    public ResponseEntity<SalesPerson> create(@RequestBody SalesPerson salesPerson, @RequestHeader HttpHeaders headers){
+        System.out.println(headers.get("User-Agent"));
+        if(salesPerson.getSalesPersonID() != null){
+            log.warn("Trying to create a user with id");
+            System.out.println("Trying to create a user with id");
+            return ResponseEntity.badRequest().build();
+        }
+        SalesPerson result = salesPersonRepository.save(salesPerson);
+        return ResponseEntity.ok(result);
+    }
+
+    @PutMapping("/api/users")
+    public ResponseEntity<SalesPerson> update(@RequestBody SalesPerson salesPerson){
+        if(salesPerson.getSalesPersonID() == null ){
+            log.warn("Trying to update a non existent user");
+            return ResponseEntity.badRequest().build();
+        }
+        if(!salesPersonRepository.existsById(salesPerson.getUserId())){
+            log.warn("Trying to update a non existent book");
+            return ResponseEntity.notFound().build();
+        }
+
+        SalesPerson result = salesPersonRepository.save(salesPerson);
+        return ResponseEntity.ok(result);
+    }
+
+    @DeleteMapping("/api/salesPerson")
+    public ResponseEntity<SalesPerson> delete(@PathVariable Long salesPersonId) {
+        if (!salesPersonRepository.existsById(salesPersonId)) {
+            log.warn("Trying to delete a non existent user");
+            return ResponseEntity.notFound().build();
+        }
+        salesPersonRepository.deleteById(salesPersonId);
+        return ResponseEntity.ok().build();
+    }
 }
--- src/main/java/com/grouzy/Backend/Controllers/UserController.java
+++ src/main/java/com/grouzy/Backend/Controllers/UserController.java
@@ -1,35 +1,74 @@
 package com.grouzy.Backend.Controllers;
 
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import com.grouzy.Backend.Entities.User;
+import com.grouzy.Backend.Repositories.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class UserController {
 
-    @GetMapping("/login")
-    public String login() {
-        return "login";
+    private UserRepository userRepo;
+    private final Logger log = LoggerFactory.getLogger(UserController.class);
+
+    public UserController(UserRepository userRepo){
+        this.userRepo = userRepo;
     }
 
-    @GetMapping("/admin")
-    public String adminEndpoint() {
-        return "Admin!";
+    @GetMapping("/api/users")
+    public List<User> findAll() {
+        return userRepo.findAll();
     }
 
-    @GetMapping("/user")
-    public String userEndpoint() {
-        return "User!";
+    @GetMapping("/api/users/{id}")
+    public ResponseEntity<User> findOneById(@PathVariable Long userId) {
+        Optional<User> userOpt = userRepo.findById(userId);
+        if (userOpt.isPresent())
+            return ResponseEntity.ok(userOpt.get());
+        else
+            return ResponseEntity.notFound().build();
     }
 
-    @GetMapping("/all")
-    public String allRolesEndpoint() {
-        return "All Roles!";
+    @PostMapping("api/users")
+    public ResponseEntity<User> create(@RequestBody User user, @RequestHeader HttpHeaders headers){
+        System.out.println(headers.get("User-Agent"));
+        if(user.getUserId() != null){
+            log.warn("Trying to create a user with id");
+            System.out.println("Trying to create a user with id");
+            return ResponseEntity.badRequest().build();
+        }
+        User result = userRepo.save(user);
+        return ResponseEntity.ok(result);
     }
 
-    @DeleteMapping("/delete")
-    public String deleteEndpoint(@RequestBody String s) {
-        return "I am deleting " + s;
+    @PutMapping("/api/users")
+    public ResponseEntity<User> update(@RequestBody User user){
+        if(user.getUserId() == null ){
+            log.warn("Trying to update a non existent user");
+            return ResponseEntity.badRequest().build();
+        }
+        if(!userRepo.existsById(user.getUserId())){
+            log.warn("Trying to update a non existent book");
+            return ResponseEntity.notFound().build();
+        }
+
+        User result = userRepo.save(user);
+        return ResponseEntity.ok(result);
+    }
+
+    @DeleteMapping("/api/users")
+    public ResponseEntity<User> delete(@PathVariable Long userId){
+        if (!userRepo.existsById(userId)) {
+            log.warn("Trying to delete a non existent user");
+            return ResponseEntity.notFound().build();
+        }
+        userRepo.deleteById(userId);
+        return ResponseEntity.ok().build();
     }
 }
